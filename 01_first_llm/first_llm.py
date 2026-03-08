7280ad1d224105d6315a305bae9b281898ff00aa
Ahmad  Alkhawaldeh <kha.ahmad2003@gmail.com>
2026-03-08 03:15:31 +0300

    Update first_llm.py

--- 01_first_llm/first_llm.py
+++ 01_first_llm/first_llm.py
@@ -1,6 +1,9 @@
 from groq import Groq
+from dotenv import load_dotenv
+import os
 
-client = Groq(api_key="your_api_key_here")
+load_dotenv()
+client = Groq(api_key=os.getenv("GROQ_API_KEY"))
 
 response = client.chat.completions.create(
     model="llama-3.3-70b-versatile",
